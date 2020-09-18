ac1532b6d46f92acb94e9759a9a848b8eda5e16e
Suraj Gupta <surajgupta268@gmail.com>
2020-09-18 20:58:48 +0530

    Add files via upload

---
+++ week2/Functional Comp/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+//import { Navbar, NavbarBrand } from "reactstrap";
+import Main from './components/MainComponent';
+import { DISHES } from "./shared/dishes";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+      <Main />
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ week2/ReactRouter/src/components/MainComponent.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+//import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+//class component
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES,
+        
+    };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId});
+  }
+
+  render() {
+
+    const HomePage = () => {
+        return(
+            <Home />
+            );
+    }
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+        </Switch>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default Main;
